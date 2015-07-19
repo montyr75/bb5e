970c17ae12b35619976eb39c46818cd57c9cd97f
Rachel Ward <rachelward_9@hotmail.com>
2015-07-18 21:40:48 -0600

    Added functionality for Custom and Size

--- lib/client/player/views.dart
+++ lib/client/player/views.dart
@@ -17,8 +17,10 @@ class PlayerInitiativeView implements View {
   InputElement meleeLW;
   InputElement meleeTwoHand;
   InputElement rangedLoading;
-  InputElement creatureSize;
-  InputElement custom;
+  CheckboxInputElement creatureSize;
+  SelectElement creatureSizeSel;
+  CheckboxInputElement custom;
+  SelectElement customSel;
 
   PlayerInitiativeView(PlayerModel this._model) {
     _getUIReferences();
@@ -31,6 +33,10 @@ class PlayerInitiativeView implements View {
     dexMod = querySelector("#dex-mod");
     spell = querySelector("#spell-cb");
     spellLevel = querySelector("#spell-level-sel");
+    creatureSize = querySelector("#creature-size-cb");
+    creatureSizeSel = querySelector("#creature-size-sel");
+    custom = querySelector("#custom-cb");
+    customSel = querySelector("#custom-sel");
   }
 
   void _setupListeners() {
@@ -40,6 +46,35 @@ class PlayerInitiativeView implements View {
       }
       else {
         _model.init.removeMod(_model.gameModel.initiativeTotalMods[2]);
+      }
+    }
+
+    //Worst function ever.
+    void _setCreatureSizeMod(Event event) {
+      if (creatureSize.checked) {
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[7]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[8]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[9]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[10]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[11]);
+        _model.init.addMod(_model.gameModel.initiativeTotalMods[int.parse(creatureSizeSel.value)].clone());
+      }
+
+      else {
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[7]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[8]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[9]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[10]);
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[11]);
+      }
+    }
+
+    void _setCustomMod(Event event) {
+      if (custom.checked) {
+        _model.init.addMod(_model.gameModel.initiativeTotalMods[13].clone()..value = int.parse(customSel.value));
+      }
+      else {
+        _model.init.removeMod(_model.gameModel.initiativeTotalMods[13]);
       }
     }
 
@@ -59,6 +94,12 @@ class PlayerInitiativeView implements View {
 
     spell.onChange.listen(_setSpellMod);
     spellLevel.onChange.listen(_setSpellMod);
+
+    creatureSize.onChange.listen(_setCreatureSizeMod);
+    creatureSizeSel.onChange.listen(_setCreatureSizeMod);
+
+    custom.onChange.listen(_setCustomMod);
+    customSel.onChange.listen(_setCustomMod);
 
     //Needs to check input first. This is not implemented yet.
 //    d20.onInput.listen((Event event) {
