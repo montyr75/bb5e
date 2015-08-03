48863b708d560f3f9f670913a9cd1c220fb93b05
montyr75 <montyr75@gmail.com>
2015-08-03 17:18:33 -0600

    adding jquery

--- web/dm.dart
+++ web/dm.dart
@@ -1,25 +1,31 @@
 import 'dart:html' as HTML;
 import 'package:firebase/firebase.dart' as FB;
-import 'package:bb5e/client/client_connection_manager.dart';
-import 'package:bb5e/comms/comms.dart';
-import 'package:bb5e/comms/message.dart';
+//import 'package:bb5e/client/client_connection_manager.dart';
+//import 'package:bb5e/comms/comms.dart';
+//import 'package:bb5e/comms/message.dart';
 
-ClientConnectionManager ccm = new ClientConnectionManager();
+//ClientConnectionManager ccm = new ClientConnectionManager();
 
 Map initModel;
 
 void main() {
-  ccm.connectToServer(SERVER_IP, SERVER_PORT);
+//  ccm.connectToServer(SERVER_IP, SERVER_PORT);
 
-  ccm.onMessage.listen((Message msg) {
-    initModel = msg.payload;
+//  ccm.onMessage.listen((Message msg) {
+//    initModel = msg.payload;
+//
+//    initModel.keys.forEach((String charName) {
+//      HTML.querySelector('#${charName.toLowerCase()}-init').text = initModel[charName].toString();
+//    });
+//  });
+//
+//  HTML.querySelector('#get-btn').onClick.listen((HTML.Event event) {
+//    ccm.sendMessage("DM", Message.GET_INIT);
+//  });
 
-    initModel.keys.forEach((String charName) {
-      HTML.querySelector('#${charName.toLowerCase()}-init').text = initModel[charName].toString();
-    });
-  });
+  FB.Firebase ref = new FB.Firebase("https://incandescent-heat-2470.firebaseio.com/characterData/orsa");
 
-  HTML.querySelector('#get-btn').onClick.listen((HTML.Event event) {
-    ccm.sendMessage("DM", Message.GET_INIT);
+  ref.onValue.listen((FB.Event event) {
+    print(event.snapshot.val());
   });
 }
