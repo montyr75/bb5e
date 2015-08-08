e9d0f179e818edbfd6c45f9e2e6a730223b20c49
montyr75 <montyr75@gmail.com>
2015-08-08 01:47:12 -0600

    fixed build bug and Firebase JS bug

--- lib/models/mod.dart
+++ lib/models/mod.dart
@@ -31,18 +31,33 @@ class Mod<T> {
   }
 
   Map toMap() {
-    return {
-      "id": id,
-      "level": level,
-      "name": name,
-      "type": type,
-      "subtype": subtype,
-      "source": source,
-      "affectedStat": affectedStat,
-      "value": value,
-      "description": description,
-      "ref": ref
-    };
+    // in JS, null values in here end up as undefined and cause Firebase.set to fail
+    Map map = {};
+    if (id != null) map['id'] = id;
+    if (level != null) map['level'] = level;
+    if (name != null) map['name'] = name;
+    if (type != null) map['type'] = type;
+    if (subtype != null) map['subtype'] = subtype;
+    if (source != null) map['source'] = source;
+    if (affectedStat != null) map['affectedStat'] = affectedStat;
+    if (value != null) map['value'] = value;
+    if (description != null) map['description'] = description;
+    if (ref != null) map['ref'] = ref;
+
+    return map;
+
+//    return {
+//      "id": id,
+//      "level": level,
+//      "name": name,
+//      "type": type,
+//      "subtype": subtype,
+//      "source": source,
+//      "affectedStat": affectedStat,
+//      "value": value,
+//      "description": description,
+//      "ref": ref
+//    };
   }
 
   Mod clone() => new Mod.fromMap(toMap());
